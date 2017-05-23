637231e855f51b88e86796ec6c9dd77e97baef53
Kateryna Bondareva <kbondareva@gmail.com>
2017-05-23 19:33:11 -0400

    Old School Java Scripting

--- kateryna/values.js
+++ kateryna/values.js
@@ -1,3 +1,20 @@
 // This is the file for values, expressions and statement
 // This is a single line comment in javascript
-console.log('It works');
+// values - what is a value?
+// 3 -number
+// "a" -string
+// true -boolean, false -boolean
+// [] - array
+// {} - object
+
+//Values and Variables
+
+// Old School Java Scripting
+var x = 3; // I have assigned the variable x the value of 3
+var y = "a"; // I have assigned the variable y the value of 'a'
+var z = []; // I have assigned the variable z the value of an empty array
+var a ={}; // I have assigned the variable a the value of empty object
+console.log (x);
+console.log (y);
+console.log (z);
+console.log (a);
