d3b644dc8931ea713a650d71ef35decad99d500f
Angela Graves <algraves78@gmail.com>
2017-05-23 19:52:30 -0400

    added const and let variable declaration and demonstrated the difference between const and let

--- angie/values.js
+++ angie/values.js
@@ -1,3 +1,30 @@
 // This is the file for Values, Expressions and Statements
 // This is a single line comment in JavaScript
-console.log('It works!');
+// values - What is a value?
+// 3 - number
+// "a" - string
+// true - boolean => false - boolean
+// [] - array
+// {} - object
+
+// Values and Variables
+// Old School JavaScript
+var x = 3; // I have assigned the variable x the value of 3
+var y = "a"; // I have assined the variable y the value of a
+var z = []; // I have assigned the variable z the value of an empty array
+var a = {}; // I have assigned the variable a the value of an empty object
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+// constant bindings
+const taxRate = 0.065;
+console.log(taxRate);
+// taxRate = 300.065; // Cannot do this because this is a reassignment of a constant
+// new browsers for constant
+
+// let bindings - new to the JavaScript
+let myFavoriteColor = 'blue';
+myFavoriteColor = 'green'; // this is allowed because let allows reassignment
+console.log(myFavoriteColor);
