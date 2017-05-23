bf76972a7b7a7464d22ab262f087e2f22a2fce52
Barb <barbndc@gmail.com>
2017-05-23 19:32:43 -0400

    wrote some console logs to demonstrate values

--- barb/values.js
+++ barb/values.js
@@ -1,3 +1,26 @@
 // This is the file for values, expressions and statements
 
-console.log('it works!');
+//values --what is a value?
+// 3 - number
+// "a" - string
+// true - boolean => false
+// [] - array
+// {} - object
+
+//values and variables
+// Old School Javascript
+
+var x = 3; // i have assigned the variable x the value of 3
+
+// = is assign  == is equality
+
+var y = "a"; // i have assigned the variable y the value of a
+
+var z = []; // i have assigned the variable z the value of an empty array
+
+var a = {}; // i have assigned the variable a the value of an empty object
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
