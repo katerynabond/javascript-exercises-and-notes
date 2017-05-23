83d8f7097dd5b88c4e73b98876ae58651704a7fe
Gizelle Fletcher <zelfletch@gmail.com>
2017-05-23 19:34:14 -0400

    added different value type values and how basic old school variables work

--- gizelle/values.js
+++ gizelle/values.js
@@ -1,4 +1,20 @@
 // This is the file for values, expressions, and statements
 // this is a single line comment in JavaScript
+// Values-What is a value?
+// 3 - number
+// "a" - string
+// true/false - boolean
+// [] - array
+// {} - object
 
-console.log('It works');
+// values and variables
+// old school JavaScript
+var x = 3; //I have assigned the variable x the value of 3
+var y = "a"; //I have assigned the variable y the value of "a"
+var z = []; //I have assigned the variable z the value of an empty array
+var a = {}; // I have assigned the variable a the value of an empty object
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
