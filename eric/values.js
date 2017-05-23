51c0b1b616b3eeba3ad53e3ade687050de65b178
Eric Hosford <eric.hosford@gmail.com>
2017-05-23 19:52:35 -0400

    added const and let variable declarations and demonstrated the differnece bertween const and let

--- eric/values.js
+++ eric/values.js
@@ -11,10 +11,21 @@
 // Old School Javascript
 var x = 3; // I have assigned the variable x the value of 3
 var y = "a"; // I have sssigned the variable y the value of a
-var z = [a,'s']; // I have assigned the variable z the value of an empty array
+var z = []; // I have assigned the variable z the value of an empty array
 var a = {}; // I have assigned the variable z the value of an empty object
 
 console.log(x);
 console.log(y);
 console.log(z);
 console.log(a);
+
+// Constant bindings
+ const taxRate = 0.065;
+ console.log(taxRate);
+// taxRate = 54; //Cannot reassign after started in "newer" browsers
+
+//let bindings
+let myFavoriteColor = 'blue';
+myFavoriteColor = 'green'; //let allows reassignment within block scope
+
+console.log(myFavoriteColor);
