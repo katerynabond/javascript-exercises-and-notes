46c5dfe9c41385f0ecafaea65605789fd13a6752
Julia Dahm <jukabrown@gmail.com>
2017-05-23 19:35:38 -0400

    different data type values and how basic old shool variables work

--- values.js
+++ values.js
@@ -1,3 +1,19 @@
 // This is the file for Values, Expressions and Statements
 // This is a single line comment in Javascript
-console.log('It works!');
+// values - What is a value?
+// 3 - number
+// "a" - string
+// true - boolean => false - boolean
+// [] - array
+// {} - object
+
+// Values and Variables
+//Old school JS
+//Varibale declarations
+var x = 3;
+var y = "a";
+var z = [];
+
+console.log(x);
+console.log(y);
+console.log(z);
