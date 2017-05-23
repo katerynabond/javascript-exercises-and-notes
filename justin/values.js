2aa54d9dd6df92726e3c1bb8294f54a4b71e2393
Justin Dennison <justin1dennison@gmail.com>
2017-05-23 19:34:14 -0400

    different data type values and how basic old school variables work

--- justin/values.js
+++ justin/values.js
@@ -1,3 +1,32 @@
 // This is the file for Values, Expressions and Statements
 // This is a single line comment in JavaScript
-console.log('It works!');
+//values - What is a value?
+// 3 - number
+// "a" - string
+// true - boolean => false - boolean
+// [] - array
+// {} - object
+
+// Values and Variables
+//Old School JavaScript
+var x = 3; // I have assigned the variable x
+              //the value of 3
+var y = "a"; // I have assigned the variable y
+              // the value of 'a';
+var z = []; // I have assigned the variable z
+              // the value of an empty array;
+var a = {}; // I have assigned the variable a
+            // the value of an empty object;
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
+
+
+
+
+
+
+
+//
