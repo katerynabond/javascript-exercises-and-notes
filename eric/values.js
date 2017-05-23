3f1112c16185614e664016d0effcd240b0bd3721
Eric Hosford <eric.hosford@gmail.com>
2017-05-23 19:34:07 -0400

    Listed and wrote different types of values to console

--- eric/values.js
+++ eric/values.js
@@ -1,2 +1,20 @@
 // This is the file for Values, Expressions and Statements
-console.log('It Works!');
+
+// Values - What is a value?
+// 3 - number
+// "a" - string
+// true - boolean => false - boolean
+// [] - array
+// {} - object
+
+// Values and Variables
+// Old School Javascript
+var x = 3; // I have assigned the variable x the value of 3
+var y = "a"; // I have sssigned the variable y the value of a
+var z = [a,'s']; // I have assigned the variable z the value of an empty array
+var a = {}; // I have assigned the variable z the value of an empty object
+
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(a);
